97e32e1114dd79277379d7f70b19543ab44408c6
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-05 21:14:03 +0300

    Fix Task02

--- src/main/java/com/epam/rd/autotasks/task02/Task02.java
+++ src/main/java/com/epam/rd/autotasks/task02/Task02.java
@@ -3,8 +3,23 @@ package com.epam.rd.autotasks.task02;
 public class Task02 {
 
 	public static int maximumDistance(int[] array) {
-		// TODO: Implement this method.
-		return 0;
+		if (array == null || array.length == 0 || array.length ==1) return 0;
+		int maxIndex = 0;
+		int maxN = array [maxIndex];
+		for (int i=1; i < array.length; i++) {
+			if (array[i] > maxN) {
+				maxN = array[i];
+				maxIndex = i;
+			}
+		}
+		int maxLastIndex = 0;
+		for (int j=1; j < array.length; j++) {
+			if (array[j] == maxN) {
+				maxLastIndex = j;
+			}
+		}
+
+		return maxLastIndex - maxIndex;
 	}
 
 	public static void main(String[] args) {
